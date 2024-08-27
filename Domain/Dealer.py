0159dd6d9ffdc201a6b50f8135738257e35755c9
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-08-27 20:13:55 +0200

    moved create deck method to cards class and added deal cards method

--- Domain/Dealer.py
+++ Domain/Dealer.py
@@ -3,27 +3,12 @@
 class Dealer:
     def __init__(self, players, cards):
         super().__init__()
-        self.deck = cards
+        self.deck = cards.deck
+        self.discard_pile = cards.discard_pile
         self.players = players
 
-    def create_deck(self):
-        for color in color_cards:
-            for i in range(2):
-                for j in range(1, 10):
-                    self.deck.append((color, j))
-                for special in special_cards:
-                    self.deck.append((color, special))
-        for i in range(4):
-            for wild in wild_cards:
-                self.deck.append(("wild", wild))
-        random.shuffle(self.deck)
-        self.discard_pile.append(self.deck.pop())
-
-    def deal_card(self):
+    def deal_cards(self):
         for player in self.players:
             for _ in range(7):
-                player.players_cards().append(self.deck[0])
-                self.discard_pile.append(self.deck.pop())
-            
-        
-
+                player.add_players_card(self.deck.pop())
+        self.discard_pile.append(self.deck.pop())
