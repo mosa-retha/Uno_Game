d9e410f2084e2fdd8fea2685bf3955462d9942f5
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-08-27 20:17:07 +0200

    added threading functions to help iteration every player in order

--- Server/GamePlay.py
+++ Server/GamePlay.py
@@ -7,12 +7,20 @@ class GamePlay(threading.Thread):
         super().__init__()
         self.socket = _socket
         self.client_address = address
+        self.player = player
+        self.ready = threading.Event()
+        self.server = server
 
     def run(self):
         start = True
         print("Welcome to UNO!")
+
         while start:
-            message = "what do you want to do?\n"
-            self.socket.send(message.encode('utf-8'))
-            data = self.socket.recv(1024).decode('utf-8')
-            print(data)
+            self.ready.wait()
+            message = "Your turn to play!" + self.player.name
+            self.client_socket.sendall(message.encode())
+            data = self.client_socket.recv(1024).decode('utf-8')
+            print(f"Received from client: {data}")
+            self.server.notify_done(self)
+
+
