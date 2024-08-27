e44cf6d5493f177b44a9b5a33df8820227433601
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-08-27 15:30:52 +0200

    gameplay as runnable for Server class

---
+++ Server/Server.py
@@ -0,0 +1,29 @@
+import socket
+
+from Server.GamePlay import GamePlay
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = 'localhost'
+port = 12345
+
+server_socket.bind((host, port))
+
+server_socket.listen()
+
+print(f"Server listening on {host}:{port}...")
+start = True
+while True:
+    client_socket, addr = server_socket.accept()
+    print(f"Got a connection from {addr}")
+
+    message = "Welcome to UNO!" + "\n"
+    client_socket.send(message.encode('utf-8'))
+
+
+    data = client_socket.recv(1024).decode('utf-8')
+
+    print(f"Received from client: {data}")
+
+    thread = GamePlay(client_socket, addr)
+    thread.start()
