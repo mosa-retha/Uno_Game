3755de9ad4012142120a6ac391ad4dca6639995f
Mosa Moime <mmoime023@student.wethinkcode.co.za>
2024-08-29 18:03:49 +0200

    started using concurrent.futures to handle threads management and client communication instead of threading and lock

--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
 import socket
-import threading
 
 from Domain.Cards import Cards
 from Domain.Player import Player
@@ -8,7 +8,7 @@ from GamePlay import GamePlay
 
 
 class Server:
-    """Server class that listens for incoming connections and starts the game."""""
+    """Server class that listens for incoming connections and starts the game."""
 
     def __init__(self):
         self.current_client_index = 0
@@ -17,8 +17,9 @@ class Server:
         self.port = 12345
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(2)
-        self.lock = threading.Lock()
+        self.executor = ThreadPoolExecutor(max_workers=2)
         self.clients = []
+        self.cards = Cards()
 
     def start_server(self):
         """Start the server and listen for incoming connections."""
@@ -34,11 +35,11 @@ class Server:
 
             print(f"Received from client: {data}")
 
-            thread = GamePlay(client_socket, addr, self, Player(data))
-            thread.start()
+            player = Player(data)
+            game_play = GamePlay(client_socket, addr, self, player, self.cards)
+            self.executor.submit(game_play.run)
 
-            with self.lock:
-                self.clients.append(thread)
+            self.clients.append(game_play)
 
             if len(self.clients) == 2:
                 print("Game is starting")
@@ -47,33 +48,27 @@ class Server:
 
     def start_round(self):
         """Start a new round."""
-        with self.lock:
-            if self.clients:
-                current_client = self.clients[self.current_client_index]
-                print(f"Starting turn for {current_client.player.name} (index: {self.current_client_index})")
-                current_client.ready.set()
-            else:
-                print("No clients connected.")
+        if self.clients:
+            current_client = self.clients[self.current_client_index]
+            print(f"Starting turn for {current_client.player.name} (index: {self.current_client_index})")
+            current_client.ready.set()
+        else:
+            print("No clients connected.")
 
     def notify_done(self, client):
         """Notify the server that the client is done with their turn."""
-        with self.lock:
-            print(f"{client.player.name} has finished their turn.")
-            client.ready.clear()
-            self.current_client_index += 1
-            if self.current_client_index == len(self.clients):
-                self.current_client_index = 0
-            print(f"Next client index: {self.current_client_index}")
-            self.start_round()
-            print("start_round was called successfully.")
-
-
-
+        print(f"{client.player.name} has finished their turn.")
+        client.ready.clear()
+        self.current_client_index += 1
+        if self.current_client_index == len(self.clients):
+            self.current_client_index = 0
+        print(f"Next client index: {self.current_client_index}")
+        self.start_round()
+        print("start_round was called successfully.")
 
     def deal(self):
         """Deal cards"""
-        cards = Cards()
-        dealer = Dealer([client.player for client in self.clients], cards)
+        dealer = Dealer([client.player for client in self.clients], self.cards)
         dealer.deal_cards()
         for client in self.clients:
             client.client_socket.sendall(f"Your cards: {client.player.players_cards}".encode('utf-8'))
